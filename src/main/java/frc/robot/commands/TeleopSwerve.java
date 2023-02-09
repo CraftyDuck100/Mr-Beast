a2328d106a1cd762999640d80bd6e38582404d66
CraftyDuck100 <61465936+CraftyDuck100@users.noreply.github.com>
2023-02-09 15:35:35 -0600

    Update TeleopSwerve.java

--- src/main/java/frc/robot/commands/TeleopSwerve.java
+++ src/main/java/frc/robot/commands/TeleopSwerve.java
@@ -37,8 +37,8 @@ public class TeleopSwerve extends CommandBase {
 
     @Override
     public void execute() {
-        double yAxis = -controller.getRawAxis(translationAxis);  //Flips the controller values as needed and gets the value of each axis
-        double xAxis = -controller.getRawAxis(strafeAxis);
+        double yAxis = -controller.getRawAxis(translationAxis) * 0.2;  //Flips the controller values as needed and gets the value of each axis
+        double xAxis = -controller.getRawAxis(strafeAxis) * 0.2;
         double rAxis = -controller.getRawAxis(rotationAxis);
         
         // Apply deadbands from constants file to each axis
